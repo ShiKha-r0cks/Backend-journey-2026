a1558710b9e7f6e7d78022e19509960ad737fa0d
Shikha Tripathi <shikhutripathi19@gmail.com>
2026-02-26 23:48:53 +0530

    practice one concept

--- Practice/list_day4.py
+++ Practice/list_day4.py
@@ -32,3 +32,12 @@ for i in Number:
      total+=i
 avg=total/len(Number)
 print(max,min,avg)
+# reverse a list
+numbers = [10, 20, 30, 40, 50]
+
+reversed_list = []
+
+for i in range(len(numbers)-1, -1, -1):
+    reversed_list.append(numbers[i])
+
+print(reversed_list)
